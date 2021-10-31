3b2888a49fb508ddc98088d3c1bfae72b533d2ad
CrisNguyen <nguyenvantrieu.qngdp@gmail.com>
2021-10-31 10:34:51 +0700

    feat: student login

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
+  has_many :student_courses
+  has_many :courses, through: :student_courses
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {minimum: 5, maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
